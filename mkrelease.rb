6b26c1c11b74d96bca273a880720901b5efbdeaa
Ilmari Heikkinen <kig@manifold.fhtr.org>
2007-09-22 13:28:19 +0300

    mkrelease

---
+++ mkrelease.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+url = "http://dark.fhtr.org/repos"
+libname = 'thumbnailer'
+version = Thumbnailer::VERSION
+
+$LOAD_PATH.unshift "lib"
+require './lib/#{libname}.rb'
+require 'fileutils'
+
+dn = "#{libname}-#{version}"
+FileUtils.mkdir(dn)
+%w(lib bin README INSTALL LICENSE setup.rb).each{|fn|
+  FileUtils.cp_r(fn, dn)
+}
+`tar zcf #{dn}.tar.gz #{dn}`
+FileUtils.rm_r(dn)
+s = <<-EOF
+tarball: #{url}/#{libname}/#{dn}.tar.gz
+git: #{url}/#{libname}
+
+
+#{File.read('README')}
+EOF
+File.open("release_msg-#{version}.txt", "w"){|f| f.write s}
